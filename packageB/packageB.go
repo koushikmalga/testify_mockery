d8a70e08460d145e4aeaf3b547a04bce5030e983
koushikmalga <koushik.malga@nutanix.com>
2023-08-21 12:26:13 +0530

    Update packageB.go

--- packageB/packageB.go
+++ packageB/packageB.go
@@ -1,16 +1,20 @@
 package packageB
 
+//packageB with an external dependency on packageC
+
 import (
 	"fmt"
 	"testify_mockery/packageC"
 )
 
+// An interface with 3 methods
 type BIfc interface {
 	Sum(int, int) int
 	UseC(int, int) int
 	Prod(int, int) int
 }
 
+// Object with packageC interface
 type B struct {
 	c packageC.CIfc
 }
@@ -24,6 +28,7 @@ func (b *B) Sum(ele1, ele2 int) int {
 	return ele1 + ele2
 }
 
+// function which depends on external dependency(package C) 
 func (b *B) UseC(ele1, ele2 int) int {
 
 	return b.c.Sub(ele1, ele2)
