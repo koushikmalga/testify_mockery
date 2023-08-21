b41d2e10e6ead3f47e319d2310cd92015c860566
koushikmalga <koushik.malga@nutanix.com>
2023-08-21 12:21:39 +0530

    Update packageA.go

--- packageA/packageA.go
+++ packageA/packageA.go
@@ -4,10 +4,12 @@ import (
 	"testify_mockery/packageB"
 )
 
+// An Interface with a method func1
 type AIfc interface {
 	func1(int, int) int
 }
 
+// object with packageB Interface
 type A struct {
 	b packageB.BIfc
 }
@@ -16,6 +18,7 @@ func NewA(b packageB.BIfc) *A {
 	return &A{b: b}
 }
 
+// func1 calling packageB
 func (a *A) func1(ele1, ele2 int) int {
 	return a.b.Sum(ele1, ele2) + a.b.UseC(ele1, ele2)
 }
